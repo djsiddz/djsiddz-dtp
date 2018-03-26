b163ec8fc774785d4bed394a9d85e49026c3a45c
Siddhesh Thadeshwar <sid@stringzweb.graphics>
2018-03-26 14:35:17 +0530

    Moved examples to separate component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { ExamplesComponent } from './examples/examples.component';
+import { DtpComponent } from './dtp/dtp.component';
+import { GetStartedComponent } from './get-started/get-started.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExamplesComponent,
+    DtpComponent,
+    GetStartedComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/examples/examples.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'djsiddz-examples',
+  templateUrl: './examples.component.html',
+  styleUrls: ['./examples.component.css']
+})
+export class ExamplesComponent implements OnInit {
+
+  title = 'Date Time Picker';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
