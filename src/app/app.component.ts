9245a2fe81dc8b54df8bbddadf19a689faff84d0
Siddhesh Thadeshwar <sid@stringzweb.graphics>
2018-03-26 13:39:20 +0530

    Adding Bootstrap; fixed prefix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'sid-root',
+  selector: 'djsiddz-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sid';
+  title = 'Date Time Picker';
 }
